84b74c0c4f882634d51260f4bc2c4ea5e0f8be77
Germán Álvarez <46670724+GermanAlvarez-IH@users.noreply.github.com>
2019-03-26 18:47:28 +0100

    Add files via upload

---
+++ server/routes/coaster-routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const Coaster = require('../models/Coaster')
+
+
+router.get('/getAllCoasters', (req, res, next) => {
+  Coaster.find()
+    .then(data => res.json(data))
+    .catch(err => console.log(err))
+})
+
+
+router.post("/postCoaster", (req, res) => {
+  Coaster.create(req.body)
+    .then(data => res.json(data))
+    .catch(err => console.log(err))
+})
+
+
+
+router.get("/getOneCoaster/:id", (req, res) => {
+  Coaster.findById(req.params.id)
+    .then(data => res.json(data))
+    .catch(err => console.log(err))
+})
+
+
+
+module.exports = router;
